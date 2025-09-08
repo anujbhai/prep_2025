e1a209b10272d9b33d5c24e88ac06529831d7c2e
Anuj Upadhyay <anuj.upadhyay@dctinc.com>
2025-09-08 23:46:55 +0530

    prep-2025-js - ts masterclass - using TS objs.

---
+++ js/cloudaffle-ts-masterclass/ctsmc-002-obj-ts/index.ts
@@ -0,0 +1,23 @@
+/* Using type alias for objects */
+type Post = {
+    title: string;
+    content: string;
+    date: Date;
+};
+
+let post: Post = {
+    title: "This is a blog post",
+    content: "This is the content of the post.",
+    date: new Date(),
+};
+
+let post2: Post = {
+    title: "This is another blog post",
+    content: "This is the content of the post.",
+    date: new Date(),
+};
+
+console.log("post title:", post.title);
+console.log("post content:", post.content);
+console.log("post date:", post.date.toLocaleString());
+console.log("post date:", post2.title);
